f7b06ad469a93a38fc8df36a692688c4d2786e88
Rubens Soares <rubens.soares@liferay.com>
2024-04-16 11:10:08 -0300

    User delete

--- src/main/java/com/javaproject/course/resources/UserResource.java
+++ src/main/java/com/javaproject/course/resources/UserResource.java
@@ -36,5 +36,11 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
